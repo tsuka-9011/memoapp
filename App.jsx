7d41be70ef6e48c7dc6b2c208ba049b654b00d1e
ab97 <user@USERnoMacBook-Pro.local>
2022-03-09 22:56:37 +0900

    install eslint

--- App.jsx
+++ App.jsx
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+      {/* eslint-disable-next-line */}
       <Text>Open up App.js to start working on your app!</Text>
+      {/* eslint-disable-next-line */}
       <StatusBar style="auto" />
     </View>
   );
